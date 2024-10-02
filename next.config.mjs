8d715ae1ebed7fdabf7b4436ed2585a6986344b7
Aziz-khan88 <aziz.rehman@crystallitedigital.com>
2024-10-02 19:55:33 +0500

    images issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,31 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: 'inhouse.cryscampus.com',
+//                 port: '',
+//                 pathname: '/**',
+//             }
+//         ]
+//     },
+//     async redirects() {
+//         return [
+//             {
+//                 source: '/mobile-app-design-company',
+//                 destination: '/top-mobile-app-development-company',
+//                 permanent: true,
+//             },
+//         ]
+//     },
+// }
+
+// export default nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'inhouse.cryscampus.com',
-                port: '',
-                pathname: '/**',
-            }
-        ]
-    },
-    async redirects() {
-        return [
-            {
-                source: '/mobile-app-design-company',
-                destination: '/top-mobile-app-development-company',
-                permanent: true,
-            },
-        ]
-    },
-}
+const nextConfig = {};
 
 export default nextConfig;
